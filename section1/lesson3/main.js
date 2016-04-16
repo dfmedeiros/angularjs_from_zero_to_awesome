82e421f2c8114cee3b8406a08d1519b16ac84aeb
Diego Medeiros <diegoo.fm@gmail.com>
2016-04-15 21:14:54 -0300

    Section 01, lesson 03

--- section1/lesson3/main.js
+++ section1/lesson3/main.js
@@ -1,3 +1,17 @@
 var app = angular.module('minmax', []);
 
-// https://minmax-server.herokuapp.com/register/'
+app.controller("MinMaxCtrl", function($scope, $http) {
+  $scope.formData = {};
+
+  $scope.onSubmit = function() {
+    console.log("Hey, I'm submitted!");
+    console.log($scope.formData);
+
+    $http.post("https://minmax-server.herokuapp.com/register/", $scope.formData).
+      success(function(data) {
+        console.log(":)");
+      }).error(function(data) {
+        console.log(":(");
+      });
+  }
+});
