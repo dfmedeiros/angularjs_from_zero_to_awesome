08a3665f00b13f77827628aa4ccf3ed22bcaef7a
Diego Medeiros <diegoo.fm@gmail.com>
2016-04-17 14:42:23 -0300

    Section 03, lesson 03

--- section3/lesson3/main.js
+++ section3/lesson3/main.js
@@ -1,19 +1,19 @@
 var app = angular.module('codecraft', []);
 
 app.controller('ParentController', function ($scope, $rootScope) {
-	//$scope.name = "Parent";
-	//
-	//$scope.reset = function () {
-	//	$scope.name = "Parent";
-	//};
+  //$scope.name = "Parent";
+  //
+  //$scope.reset = function () {
+  //  $scope.name = "Parent";
+  //};
 
 });
 
 app.controller('ChildController', function ($scope, $rootScope) {
 
-	$scope.reset = function () {
-		$rootScope.name = "Reset By Child";
-	};
+  $scope.reset = function () {
+    $rootScope.name = "Reset By Child";
+  };
 
 });
 
