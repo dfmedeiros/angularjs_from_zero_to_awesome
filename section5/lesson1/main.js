c9afa389d6848d3de61e29c6cc690479f7cbf184
Diego Medeiros <diegoo.fm@gmail.com>
2016-04-18 23:46:39 -0300

    Section 05, lesson 05

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -24,6 +24,7 @@ app.controller('PersonListController', function ($scope, ContactService) {
 
   $scope.loadMore = function() {
     console.log("Load more!");
+    $scope.contacts.loadMore();
   };
 
   $scope.sensitiveSearch = function (person) {
@@ -42,18 +43,37 @@ app.service('ContactService', function (Contact) {
     'addPerson': function (person) {
       this.persons.push(person);
     },
-    'pageNumber': 1,
+    'page': 1,
     'hasMore': true,
     'isLoading': false,
     'selectedPerson': null,
     'persons': [],
     'loadContacts': function() {
-      Contact.get(function(data) {
-        console.log(data);
-        angular.forEach(data.results, function(person) {
-          self.persons.push(new Contact(person));
+      if (self.hasMore && !self.isLoading) {
+        self.isLoading = true;
+
+        var params = {
+          'page': self.page
+        };
+
+        Contact.get(params, function(data) {
+          console.log(data);
+          angular.forEach(data.results, function(person) {
+            self.persons.push(new Contact(person));
+          });
+
+          if (!data.next) {
+            self.hasMore = false;
+          }
+          self.isLoading = false;
         });
-      });
+      }
+    },
+    'loadMore': function() {
+      if (self.hasMore && !self.isLoading) {
+        self.page += 1;
+        self.loadContacts();
+      }
     }
   };
 
