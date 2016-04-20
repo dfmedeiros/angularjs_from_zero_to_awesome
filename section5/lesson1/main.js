6808c76ea49a4d2b4fe2d2f8823a531dac9e43d0
Diego Medeiros <diegoo.fm@gmail.com>
2016-04-20 20:48:01 -0300

    Section 05, lesson 17

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -56,7 +56,9 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
 
   $scope.createContact = function() {
     console.log("create contact");
-    $scope.contacts.createContact($scope.contacts.selectedPerson);
+    $scope.contacts.createContact($scope.contacts.selectedPerson).then(function() {
+      $scope.createModal.hide();
+    });
   }
 
   $scope.$watch('search', function(newVal, oldVal) {
@@ -72,7 +74,7 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
   });
 });
 
-app.service('ContactService', function (Contact) {
+app.service('ContactService', function (Contact, $q) {
 
   var self = {
     'addPerson': function (person) {
@@ -147,11 +149,20 @@ app.service('ContactService', function (Contact) {
       });
     },
     'createContact': function(person) {
+      var d = $q.defer();
       self.isSaving = true;
 
       Contact.save(person).$promise.then(function() {
         self.isSaving = false;
+        self.selectedPerson = null;
+        self.hasMore = true;
+        self.page = 1;
+        self.persons = [];
+        self.loadContacts();
+        d.resolve();
       });
+
+      return d.promise;
     }
   };
 
