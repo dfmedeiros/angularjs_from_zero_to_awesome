80b91e76be733f87b958dfacb469ff18e2e0f2f0
Diego Medeiros <diegoo.fm@gmail.com>
2016-04-25 18:54:01 -0300

    Section 07, lesson 02

--- section7/lesson1/main.js
+++ section7/lesson1/main.js
@@ -10,6 +10,16 @@ var app = angular.module('codecraft', [
   'ui.router'
 ]);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('list', {
+      url: '/',
+      templateUrl: 'templates/list.html'
+    });
+
+    $urlRouterProvider.otherwise('/');
+});
+
 app.config(function ($httpProvider, $resourceProvider, laddaProvider, $datepickerProvider) {
   $httpProvider.defaults.headers.common['Authorization'] = 'Token xxx';
   $resourceProvider.defaults.stripTrailingSlashes = false;
